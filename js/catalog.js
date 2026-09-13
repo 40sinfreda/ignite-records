5599071675cfcc6418981b0780eff3a710796d6e
40sinfreda <h.israel2008@gmail.com>
2026-09-13 15:50:27 +0300

    Wire ROCK HITS pages to hosted SVG cover

--- js/catalog.js
+++ js/catalog.js
@@ -127,7 +127,7 @@ window.IGNITE = {
       title: "IGNITE ROCK HITS",
       label: "Weekly rock",
       desc: "Billboard rock and alternative charts, plus approved independent tracks every 5 songs. Updated weekly.",
-      cover: "https://image-cdn-fa.spotifycdn.com/image/ab67706c0000da84f3a7a688625fdebc09f17d42",
+      cover: "brand/ignite-rock-hits.svg",
       spotifyId: "3s5MZEfHafUQAqFSnZIDpM",
       submit: "playlist-rock-hits.html"
     }
