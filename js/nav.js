cb6e5b39d4128f04336cdbf8f41aa517c63bec36
40sinfreda <h.israel2008@gmail.com>
2026-09-18 17:34:55 +0300

    Add tiny admin footer link on all pages

--- js/nav.js
+++ js/nav.js
@@ -6,4 +6,9 @@
       links.classList.toggle("open");
     });
   }
+  var footers = document.querySelectorAll("footer p, .footer");
+  for (var i = 0; i < footers.length; i++) {
+    if (footers[i].innerHTML.indexOf("admin.html") >= 0) continue;
+    footers[i].innerHTML += ' · <a href="admin.html" style="opacity:.35;font-size:.75rem">אדמין</a>';
+  }
 })();
