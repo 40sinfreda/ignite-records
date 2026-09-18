139c88fe6d7915ae9ab670e9ca53716277636427
40sinfreda <h.israel2008@gmail.com>
2026-09-18 20:46:48 +0300

    Add Teomic Fire album NO WITNESSES

--- js/catalog.js
+++ js/catalog.js
@@ -80,6 +80,7 @@ window.IGNITE = {
         "\u05d4\u05d0\u05e1\u05ea\u05d8\u05d9\u05e7\u05d4: \u05dc\u05d1\u05d5\u05e9 \u05d8\u05db\u05e0\u05d5\u05dc\u05d5\u05d2\u05d9 \u05de\u05d9\u05e0\u05d9\u05de\u05dc\u05d9\u05e1\u05d8\u05d9, \u05ea\u05d0\u05d5\u05e8\u05ea LED \u05d5\u05d0\u05e9 \u05d3\u05d9\u05d2\u05d8\u05dc\u05d9\u05ea \u05e2\u05dc \u05de\u05e1\u05db\u05d9\u05dd. \u05d4\u05dd \u05e0\u05de\u05e6\u05d0\u05d9\u05dd \u05d1\u05db\u05dc \u05e9\u05dc\u05d1, \u05de\u05d4\u05e4\u05e7\u05d4 \u05e2\u05d3 \u05d4\u05d4\u05e4\u05e6\u05d4."
       ],
       albums: [
+        { title: "NO WITNESSES", id: "7ab3TznxMITknzwGGI2rGA" },
         { title: "HIGH ENERGY", id: "4RikPOmKaec4BVtPx7fGhi" },
         { title: "Binary flame", id: "56p5fgf7pInYHskZa1NrZH" },
         { title: "WE ARE LIGHT", id: "5BOfdDEGNX6Rshq3R4AIAE" },
